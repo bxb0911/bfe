dd4672ef0f7596ff797e12a57b3eb8d9136ee78c
baiyanli <baiyanli@zuoyebang.com>
2021-03-30 20:54:48 +0800

    feat: code changes

--- 3.js
+++ 3.js
@@ -1,36 +1,52 @@
 // Array.prototype.flat()
 // 能否不用递归而用迭代的方式实现？
 
-const arr = [[1,1] [[2],1], [[3, [4]],1]];
+const arr = [1, [2], [3, [4]]];
+// let a = flat(arr)
+// // [1, 2, 3, [4]]
 
-let a = flat(arr)
-// [1, 2, 3, [4]]
-
-let b = flat(arr, 1)
+// let b = flat(arr, 1)
 // [1, 2, 3, [4]]
 
 let c = flat(arr, 2)
 // [1, 2, 3, 4]
 
-console.log(a)
-console.log(b)
+// console.log(a)
+// console.log(b)
 console.log(c)
 
-// [[1,1] [[2],1], [[3, [4]],1]]
-// [[[2],1], [[3, [4]],1]]
-// [[2,0], [[3, [4]],1]]
-// [[[3, [4]],1]]
-// [[3, 0], [[4], 0]]
-function flat(arr, dep = 1) {
-  let res
-  while (dep-- > 0) {
-    res = []
-    for (let i = 0; i < arr.length; i++) {
-      Array.isArray(arr[i])
-      ? res.push(...arr[i])
-      : res.push(arr[i])
+function flat(arr, depth = 1) {
+  let result = []
+  let stack = [...arr.map(item => ([item, depth]))]
+  while (stack.length > 0) {
+    const [item, depth] = stack.shift()
+    if (Array.isArray(item) && depth > 0) {
+      stack.unshift(...item.map(innerItem => ([innerItem, depth - 1])))
+    } else {
+      result.push(item)
     }
-    arr = res.slice(0)
   }
-  return res
+  return result
 }
+
+// function flat(arr, depth = 1) {
+//   // [[1,1] [[2],1], [[3, [4]],1]]
+//   // [[[2],1], [[3, [4]],1]]
+//   // [[2,0], [[3, [4]],1]]
+//   // [[[3, [4]],1]]
+//   // [[3, 0], [[4], 0]]
+  
+//   const result = []
+//   const stack = [...arr.map(item => ([item, depth]))]
+  
+//   while (stack.length > 0) {
+//     const [top, depth] = stack.pop()
+//     if (Array.isArray(top) && depth > 0) {
+//       stack.push(...top.map(item => ([item, depth - 1])))
+//     } else {
+//       result.push(top)
+//     }
+//   }
+  
+//   return result.reverse()
+// }
